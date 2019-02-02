ac441b093ff0b27795fbd29a88020777fb170216
vyskoutaris <vyskoutaris@hotmail.com>
2019-02-02 23:28:30 +0000

    Automatically backed up by Learn

--- for.rb
+++ for.rb
@@ -1,18 +1,11 @@
-=begin
+
 def using_for
 	checklist = 1..10
 	for charm in checklist
 	puts "Wingardium Leviosa"
  end
 end
-=end
 
-def using_for
- while checklist < 10
-  puts "Wingardium Leviosa"
-  using_for += 10
- end
-end
 
 
 
